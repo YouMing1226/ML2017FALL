7c691a25bf09a30b156c1972df713b7d3b92621e
YouMing1226 <32472274+YouMing1226@users.noreply.github.com>
2018-01-16 19:11:56 +0800

    Update hw4_test.sh

--- hw4/hw4_test.sh
+++ hw4/hw4_test.sh
@@ -1,2 +1,3 @@
-wget -O word2vec https://www.dropbox.com/s/b4l3r99ua3bxrc8/word2vec?dl=1
+wget -O word2vec https://www.dropbox.com/s/b4l3r99ua3bxrc8/word2vec?dl=1 
+wget -O model.h5 https://www.dropbox.com/s/yuw3wmdvnafy72u/model.h5?dl=1
 python3 train.py test --load_model model.h5 --test_path $1 --result_path $2
